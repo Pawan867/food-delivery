5baffb5ed6801506e8323eb4c5d643dea6a327ec
Pawan867 <pawan.acharya867@gmail.com>
2025-04-17 13:59:25 +0545

    Hosts declare

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Secret Key & Debug
 SECRET_KEY = 'django-insecure-*!$=7%-=d1ix6dnv)k^5m+*(#6_!ub20#n=+e8fhvd=f9oj9vk'
 DEBUG = True
-ALLOWED_HOSTS = ['.railway.app']
+# ALLOWED_HOSTS = ['.railway.app']
+ALLOWED_HOSTS = ['*', 'food-delivery.up.railway.app']
 
 # Installed Apps
 INSTALLED_APPS = [
